5eb393c688db7b00fd350dfc6cbd3e2354bd680f
Honwhy Wang <honwhy.wang@hotmail.com>
2025-01-21 00:08:43 +0800

    feat: svg for favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,6 +16,9 @@ export default defineNuxtConfig({
       meta: [
         { name: 'description', content: '一个简单易用的微信公众号Markdown浏览器插件' },
         { name: 'keywords', content: '微信公众号,Markdown,编辑器,浏览器插件,mpmd,公众号图床,哪里不会点哪里'}
+      ],
+      link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/images/logo.svg' },
       ]
     }
   },
