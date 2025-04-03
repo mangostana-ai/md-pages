8c0c16942b48515815cb2176dc0d6d569f4f7b31
Honwhy Wang <honwhy.wang@hotmail.com>
2025-04-03 22:06:27 +0800

    feat: seo meta

--- app.vue
+++ app.vue
@@ -3,3 +3,13 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+<script setup lang="ts">
+useSeoMeta({
+  title: '微信公众号Markdown编辑器',
+  ogTitle: '微信公众号Markdown编辑器',
+  description: '一款高度简洁的微信 Markdown 编辑器(及浏览器插件)：支持 Markdown 语法、色盘取色、多图上传、一键下载文档、自定义 CSS 样式、一键重置等特性',
+  ogDescription: '一款高度简洁的微信 Markdown 编辑器(及浏览器插件)：支持 Markdown 语法、色盘取色、多图上传、一键下载文档、自定义 CSS 样式、一键重置等特性',
+  ogImage: '/images/logo.svg',
+  twitterCard: 'summary_large_image',
+})
+</script>
